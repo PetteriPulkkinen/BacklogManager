fcffe0a0b344e17c0e3914db0c7f9e658d974d96
Petteri Pulkkinen <petteri.pulkkinen@aalto.fi>
2018-07-06 20:32:43 +0300

    Add dull classes for core functionalities
    Signed-off-by: Petteri Pulkkinen <petteri.pulkkinen@aalto.fi>

--- src/MCore/manager_core.py
+++ src/MCore/manager_core.py
@@ -4,3 +4,14 @@ Date 6.6.2018
 
 This file is used to contain core functionalities for the application
 '''
+
+class Manager(object):
+    pass
+
+
+class Backlog(object):
+    pass
+
+
+class Task(object):
+    pass
